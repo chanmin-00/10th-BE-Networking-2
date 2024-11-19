4e36878833741b379006c58255b3379a8de4c49e
jeongchanmin <jjw421412@naver.com>
2024-11-19 16:46:29 +0900

    Feat : JpaRepository 상속 PostRepository 추가

---
+++ backend/src/main/java/cotato/backend/domains/post/repository/PostRepository.java
@@ -0,0 +1,17 @@
+package cotato.backend.domains.post.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import cotato.backend.domains.post.entity.Post;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long>, PostCustomRepository {
+
+	@Query("SELECT p FROM Post p ORDER BY p.views DESC")
+	Page<Post> findAllByOrderByViewsDesc(Pageable pageable);
+
+}
