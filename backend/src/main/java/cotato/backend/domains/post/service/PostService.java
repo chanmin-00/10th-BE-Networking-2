a7bea5689e02d715b6920251c07f5f77ee2b99b1
jeongchanmin <jjw421412@naver.com>
2024-12-26 23:33:54 +0900

    chore : 서비스 계층에 PostDTO 적용

--- backend/src/main/java/cotato/backend/domains/post/service/PostService.java
+++ backend/src/main/java/cotato/backend/domains/post/service/PostService.java
@@ -1,22 +1,19 @@
 package cotato.backend.domains.post.service;
 
-import static cotato.backend.common.exception.ErrorCode.*;
+import static cotato.backend.domains.post.exception.PostErrorCode.*;
+import static java.util.stream.Collectors.*;
 
 import java.util.List;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import cotato.backend.common.excel.ExcelUtils;
-import cotato.backend.common.exception.ApiException;
-import cotato.backend.domains.post.converter.PostConverter;
-import cotato.backend.domains.post.dto.request.SavePostRequest;
-import cotato.backend.domains.post.dto.response.GetPostPreviewListResponse;
-import cotato.backend.domains.post.dto.response.GetPostPreviewResponse;
-import cotato.backend.domains.post.dto.response.GetPostResponse;
+import cotato.backend.domains.post.dto.PostDTO;
+import cotato.backend.domains.post.dto.PostListDTO;
 import cotato.backend.domains.post.entity.Post;
+import cotato.backend.domains.post.exception.PostException;
 import cotato.backend.domains.post.repository.PostRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -29,83 +26,69 @@ public class PostService {
 
 	private final PostRepository postRepository;
 
+	private static final int PAGE_SIZE = 10;
+
 	// 로컬 파일 경로로부터 엑셀 파일을 읽어 Post 엔터티로 변환하고 저장
 	@Transactional
-	public void saveEstatesByExcel(String filePath) {
-		if (filePath == null) {
-			throw ApiException.from(INVALID_PARAMETER);
-		}
+	public void saveEstatesByExcel(final String filePath) {
 
 		try {
 			// 엑셀 파일을 읽어 데이터 프레임 형태로 변환
-			List<Post> postList = ExcelUtils.parseExcelFile(filePath).stream().map(row -> {
-				String title = row.get("title");
-				String content = row.get("content");
-				String name = row.get("name");
-
-				return PostConverter.toPostEntity(title, content, name);
-			}).toList();
+			List<Post> postList = ExcelUtils.parseExcelFile(filePath).stream()
+				.map(PostDTO::toPostDTO)
+				.map(Post::toPostEntity)
+				.collect(toList());
 
 			// 게시글 목록 저장
-			postRepository.saveAllByJdbcTemplate(postList);
+			postRepository.saveAllPostsByJdbcTemplate(postList);
 
 		} catch (Exception e) {
 			log.error("Failed to save estates by excel", e);
-			throw ApiException.from(SAVE_EXCEL_FILE_FAILED);
+			throw PostException.from(SAVE_EXCEL_FILE_FAILED);
 		}
 	}
 
 	// 단일 게시글 저장
 	@Transactional
-	public void savePost(SavePostRequest request) {
-		if (request.getTitle() == null || request.getContent() == null || request.getName() == null) {
-			throw ApiException.from(INVALID_PARAMETER);
-		}
-
-		Post post = PostConverter.toPostEntity(request);
-
-		postRepository.save(post);
+	public void savePost(final PostDTO postDTO) {
+		postRepository.save(Post.toPostEntity(postDTO));
 	}
 
 	// 단일 게시글 조회
-	public GetPostResponse getPostById(Long id) {
+	@Transactional
+	public PostDTO getPostById(final long id) {
 
 		// 게시글이 존재하지 않을 경우 예외 처리
-		Post post = postRepository.findById(id).orElseThrow(() -> ApiException.from(POST_NOT_FOUND));
+		Post post = postRepository.findById(id).orElseThrow(() -> PostException.from(POST_NOT_FOUND));
 
 		// 조회수 증가
 		post.increaseViews();
 
-		// 조회수 증가된 게시글 저장
-		postRepository.save(post);
-
 		// DTO로 변환하여 반환
-		return PostConverter.toGetPostResponse(post);
+		return PostDTO.toPostDTO(post);
 	}
 
 	// 게시글 목록 조회, 조회수 내림차순 정렬
-	public GetPostPreviewListResponse getPostPreviewList(int page) {
-		if (page < 0) {
-			throw ApiException.from(INVALID_PARAMETER);
-		}
+	@Transactional(readOnly = true)
+	public PostListDTO getPostPreviewList(final int page) {
 
-		Pageable pageable = Pageable.ofSize(10).withPage(page);
-		Page<Post> postPage = postRepository.findAllByOrderByViewsDesc(pageable);
+		Pageable pageable = getPageable(page);
 
-		List<GetPostPreviewResponse> postPreviewList = postPage.getContent()
-			.stream()
-			.map(PostConverter::toGetPostPreviewResponse)
-			.toList();
+		return PostListDTO.toPostListDTO(postRepository.findAllByOrderByViewsDesc(pageable));
 
-		return PostConverter.toGetPostPreviewListResponse(postPreviewList, postPage.getTotalPages(),
-			postPage.getNumber());
 	}
 
 	// 단일 게시글 삭제
 	@Transactional
-	public void deletePost(Long id) {
-		Post post = postRepository.findById(id).orElseThrow(() -> ApiException.from(POST_NOT_FOUND));
-		postRepository.delete(post);
+	public void deletePost(final long id) {
+		if (!postRepository.existsById(id)) {
+			throw PostException.from(POST_NOT_FOUND);
+		}
+		postRepository.deleteById(id);
 	}
 
+	// 페이징 정보 반환
+	protected Pageable getPageable(final int page) {
+		return Pageable.ofSize(PAGE_SIZE).withPage(page);
+	}
 }
